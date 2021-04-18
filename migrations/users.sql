0701f99e943ee4fc9a22964372b8afbe0c07f184
Cyrhades <cyrhades76@gmail.com>
2021-04-18 17:15:06 +0200

    Ajout du dump de la structure SQL

---
+++ migrations/users.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(120) NOT NULL,
+  `password` varchar(255) NOT NULL COMMENT 'Hash',
+  `civility` enum('1','2') NOT NULL COMMENT '1=Homme, 2=Femme',
+  `lastname` varchar(40) NOT NULL,
+  `firstname` varchar(40) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
