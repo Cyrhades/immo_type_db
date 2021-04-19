98102b5616c58017e1b948ab25601ee586118643
Cyrhades <cyrhades76@gmail.com>
2021-04-19 12:53:28 +0200

    Ajout de la gestion des erreurs HTTP 404 et 405

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,29 @@
 let loader = require('./getFiles.js');
+let errorsHTTP = require('./errorsHTTP.js')();
 module.exports = (app) => {
 
-    app.get('/', loader().controller('Home').print);
+    app.route("/")
+        .get(loader().controller('Home').print)
+        .all(errorsHTTP.error405);
     
-    app.get('/inscription', loader().controller('Register').printForm);
-    app.post('/inscription', loader().controller('Register').processForm);
+    app.route("/inscription")
+        .get(loader().controller('Register').printForm)
+        .post(loader().controller('Register').processForm)
+        .all(errorsHTTP.error405);
    
-    app.get('/connexion', loader().controller('Authenticated').printForm);
-    app.post('/connexion', loader().controller('Authenticated').processForm);
+    app.route("/connexion")
+        .get(loader().controller('Authenticated').printForm)
+        .post( loader().controller('Authenticated').processForm)
+        .all(errorsHTTP.error405);
 
-    app.get('/deconnexion', loader().controller('Authenticated').disconnect);
+    app.route("/deconnexion")
+        .get(loader().controller('Authenticated').disconnect)
+        .all(errorsHTTP.error405);
 
-    app.get('/admin', loader().controller('Dashboard').print);
+    app.route("/admin")
+        .get(loader().controller('Dashboard').print)
+        .all(errorsHTTP.error405);
+
+    // Erreur 404
+    app.route("*").all(errorsHTTP.error404);
 };
